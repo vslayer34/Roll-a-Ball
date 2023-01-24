15bb8b7fdfa628dcc0bae6d82aa75a562b26600a
vslayer34 <mohamd.elnagar.34@gmail.com>
2023-01-24 05:43:30 +0200

    added colectables

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(Vector3.up, 50.0f * Time.deltaTime, Space.World);
+    }
+}
